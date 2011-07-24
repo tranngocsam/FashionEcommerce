3b8efbf03f5c9c1acd26986b0ca209730ac7cb98
root <root@ip-10-128-34-190.ap-southeast-1.compute.internal>
2011-07-24 13:06:04 +0000

    Re-style

---
+++ app/controllers/taxons_controller.rb
@@ -0,0 +1,23 @@
+class TaxonsController < Spree::BaseController
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+  helper :products
+
+  respond_to :html
+
+  def show
+    @taxon = Taxon.find_by_permalink!(params[:id])
+    return unless @taxon
+
+    @searcher = Spree::Config.searcher_class.new(params.merge(:taxon => @taxon.id))
+    @products = @searcher.retrieve_products
+		@show_minor_nav = true
+
+    respond_with(@taxon)
+  end
+
+  private
+
+  def accurate_title
+    @taxon ? @taxon.name : super
+  end
+end
